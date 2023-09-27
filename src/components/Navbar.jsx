3e1e219cd6771d0e0287d05213438b9bba911c4e
yetis_batuhan <batuhanyetis94@gmail.com>
2023-09-27 15:08:20 +0300

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,64 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-white text-2xl font-bold">
+      <div className="container- mx-auto flex justify-between items-center">
+        <a href="/" className="text-white text-2xl">
           BATEM News
         </a>
         <ul className="flex space-x-4">
+          
           <li>
-            <a href="/spor" className="text-white hover:underline">
-              Spor
+            <a href="/" className="text-white hover:underline">
+              GENERAL
             </a>
           </li>
           <li>
-            <a href="/finans" className="text-white hover:underline">
-              Finans
+            <a href="/" className="text-white hover:underline">
+              BUSİNESS
             </a>
           </li>
           <li>
-            <a href="/politika" className="text-white hover:underline">
-              Politika
+            <a href="/" className="text-white hover:underline">
+              SPORTS
             </a>
           </li>
+          <li>
+            <a href="/" className="text-white hover:underline">
+              TECHNOLOGY
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-white hover:underline">
+              HEALTH
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-white hover:underline">
+              SCİENCE
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-white hover:underline">
+            ENTERTAİNMENT
+            </a>
+          </li>
+        </ul>
+        <ul className="flex gap-4 text-white">
+        <li>
+        <div className="button-special text-black font-mono  w-24 h-50 border rounded-full ... text-center bg-white border-b-indigo-800">
+        <a href="/" >
+         GİRİŞ YAP
+          </a>
+        </div>
+       
+        </li>  
+        <li>     
+        <div className="button-special text-black font-mono hover:underline w-20 h-50 border rounded-full ... text-center bg-white border-b-indigo-800">
+        <a href="/Sign" >
+         ÜYE OL
+          </a>
+        </div>
+        </li> 
         </ul>
       </div>
     </nav>
