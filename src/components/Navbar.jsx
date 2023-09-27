e2c6f9109c6fcf214e72bb96b6fc1e47046a823c
yetis_batuhan <batuhanyetis94@gmail.com>
2023-09-27 23:22:35 +0300

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,33 @@ function Navbar() {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">
+        <Link to="/" className="text-white text-2xl">
           BATEM News
         </Link>
         <ul className="flex space-x-4">
           <li>
-            <Link to="/spor" className="text-white hover:underline font-bold">
-              Spor
+            <Link to="/general" className="text-white hover:underline">
+              GENERAL
+            </Link>
+          </li>
+          <li>
+            <Link to="/business" className="text-white hover:underline">
+              BUSINESS
+            </Link>
+          </li>
+          <li>
+            <Link to="/sports" className="text-white hover:underline">
+              SPORTS
+            </Link>
+          </li>
+          <li>
+            <Link to="/technology" className="text-white hover:underline">
+              TECHNOLOGY
+            </Link>
+          </li>
+          <li>
+            <Link to="/health" className="text-white hover:underline">
+              HEALTH
             </Link>
           </li>
           <li>
@@ -23,6 +43,22 @@ function Navbar() {
             <Link to="/entertainment" className="text-white hover:underline">
               ENTERTAINMENT
             </Link>
+          </li>
+        </ul>
+        <ul className="flex gap-4 text-white">
+          <li>
+            <div className="button-special text-black font-mono w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
+              <Link to="/login">
+                LOGİN
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div className="button-special text-black font-mono hover:underline w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
+              <Link to="/signup">
+                SİGN UP
+              </Link>
+            </div>
           </li>
         </ul>
       </div>
