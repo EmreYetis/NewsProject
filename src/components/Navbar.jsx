a5c31323696201d74a3afd6af2c86c9b459779a2
Emre Yetis <emreyetis955@gmail.com>
2023-10-09 13:28:09 +0300

    Navbar Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center gap-2">
         <Link to="/" className="text-white text-2xl">
           BATEM News
         </Link>
-        <div className="hidden md:block">
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="text-white hover:underline">
-              GENERAL
-            </Link>
-          </li>
-          <li>
-            <Link to="/business" className="text-white hover:underline">
-              BUSINESS
-            </Link>
-          </li>
-          <li>
-            <Link to="/spor" className="text-white hover:underline">
-              SPORTS
-            </Link>
-          </li>
-          <li>
-            <Link to="/technology" className="text-white hover:underline">
-              TECHNOLOGY
-            </Link>
-          </li>
-          <li>
-            <Link to="/health" className="text-white hover:underline">
-              HEALTH
-            </Link>
-          </li>
-          <li>
-            <Link to="/science" className="text-white hover:underline">
-              SCIENCE
-            </Link>
-          </li>
-          <li>
-            <Link to="/entertainment" className="text-white hover:underline">
-              ENTERTAINMENT
-            </Link>
-          </li>
-        </ul>
+        <div className="md:hidden">
+          <button onClick={toggleNavbar} className="text-white hover:underline">
+            Menu
+          </button>
         </div>
-        <ul className="flex gap-4 text-white">
+        <div className={`md:flex md:space-x-4 ${isOpen ? "block" : "hidden"}`}>
+          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 sm:text-sm ">
+            <li>
+              <Link to="/" className="text-white hover:underline">
+                GENERAL
+              </Link>
+            </li>
+            <li>
+              <Link to="/business" className="text-white hover:underline">
+                BUSINESS
+              </Link>
+            </li>
+            <li>
+              <Link to="/sports" className="text-white hover:underline">
+                SPORTS
+              </Link>
+            </li>
+            <li>
+              <Link to="/technology" className="text-white hover:underline">
+                TECHNOLOGY
+              </Link>
+            </li>
+            <li>
+              <Link to="/health" className="text-white hover:underline">
+                HEALTH
+              </Link>
+            </li>
+            <li>
+              <Link to="/science" className="text-white hover:underline">
+                SCIENCE
+              </Link>
+            </li>
+            <li>
+              <Link to="/entertainment" className="text-white hover:underline">
+                ENTERTAINMENT
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <ul className="lg:flex gap-4 text-white">
           <li>
             <div className="button-special text-black font-mono w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
-              <Link to="/pages/Login">
-                LOGİN
-              </Link>
+              <Link to="/pages/Login">LOG IN</Link>
             </div>
           </li>
           <li>
-            <div className="button-special text-black font-mono hover:underline w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
-              <Link to="/signup">
-                SİGN UP
-              </Link>
+            <div className="sm:mt-1 lg:mt-0 button-special text-black font-mono hover:underline w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
+              <Link to="/signup">SIGN UP</Link>
             </div>
           </li>
         </ul>
