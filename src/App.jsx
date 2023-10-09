0874f28c998a8bfd00eef583a27d119e10d00c3d
Emre Yetis <emreyetis955@gmail.com>
2023-10-09 12:46:10 +0300

    All categorys added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import NewsBitcoin from "./components/NewsBitcoin";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Spor from "./components/Spor";
+import Spor from "./components/categorys/Spor";
 import Login from "./components/pages/Login";
+import Busness from "./components/categorys/Busness";
+import Technology from "./components/categorys/Technology";
+import Health from "./components/categorys/Health";
+import Science from "./components/categorys/Science";
+import Entertainment from "./components/categorys/Entertainment";
 
 function App() {
   return (
@@ -15,16 +20,19 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Newss />} />
+            <Route path="business" element={<Busness />} />
             <Route path="/spor" element={<Spor />} />
-            
+            <Route path="/technology" element={<Technology />} />
+            <Route path="/health" element={<Health />} />
+            <Route path="/science" element={<Science />} />
+            <Route path="/entertainment" element={<Entertainment />} />
+            Science
           </Routes>
           <Routes>
-          
-          <Route path="/pages/login" element={<Login />} />
+            <Route path="/pages/login" element={<Login />} />
           </Routes>
           <Routes>
             <Route path="/" element={<NewsBitcoin />} />
-            
           </Routes>
 
           <Footer />
---
+++ src/components/categorys/Science.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+function Science() {
+  const key = process.env.REACT_APP_API_KEY;
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function getNews() {
+      try {
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${key}`
+        );
+        console.log(response);
+        setNews(response.data.articles);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
+
+    getNews();
+  }, []); // Boş bağımlılık dizisi ile sadece bir kere çalışmasını sağlayın
+
+  const limitContent = (content, maxLength) => {
+    if (content !== null && content.length > maxLength) {
+      return content.substring(0, maxLength) + "...";
+    } else {
+      return content;
+    }
+  };
+
+  return (
+    <div className="flex flex-wrap justify-around w-3/4 m-auto">
+      {news.map((item, index) => (
+        <div
+          key={index}
+          className="border border-gray-300 p-5 m-8 w-72 text-center rounded-lg shadow-md flex flex-col"
+        >
+          <h1 className="text-2xl mt-4">{item.title}</h1>
+          <p className="text-base text-gray-500">{item.author}</p>
+          <hr className="mt-2" />
+
+          {/* Boş bir div ekleyerek tüm kartların aynı hizada olmasını sağlayın */}
+          <div className="flex-grow"></div>
+
+          <a
+            className="text-base font-semibold text-red-300"
+            href={item.url}
+            target="_blank"
+          >
+            Click for details...
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Science;
