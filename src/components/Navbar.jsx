d037c9d7f1935158abb316eb0601ea1a6ffa44b3
Emre Yetis <emreyetis955@gmail.com>
2023-10-27 14:37:23 +0300

    Navbarr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,47 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
+  const [handleMenu, setHandleMenu] = useState(false);
 
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center gap-2">
         <Link to="/" className="text-white text-2xl">
-          BATEM News
+          <div className="flex">
+            <div>
+              <button
+                className="btn pr-2"
+                onClick={() => setHandleMenu(!handleMenu)}
+              >
+                <i className="fa fa-bars"></i>
+              </button>
+              <aside
+                className={`bg-blue-500  text-white p-6 min-h-screen lg:w-64 lg:block fixed top-0 left-0 transition-transform transform ${
+                  handleMenu ? "translate-x-0" : "-translate-x-full"
+                }`}
+              >
+                <i
+                  className="fa fa-bars mb-7 "
+                  onClick={() => setHandleMenu(!handleMenu)}
+                ></i>
+                <ul>
+                  <Link to="/" className="text-white hover:underline">
+                    GENERAL
+                  </Link>
+                  <li className="mb-2">BUSINESS</li>
+                  <li className="mb-2">SPORTS</li>
+                  <li className="mb-2">TECHNOLOGY</li>
+                  <li className="mb-2">HEALTH</li>
+                  <li className="mb-2">SCIENCE</li>
+                  <li>ENTERTAINMENT</li>
+                </ul>
+              </aside>
+            </div>
+            <h1>BATEM News</h1>
+          </div>
         </Link>
-        <div className="md:hidden">
-          <button onClick={toggleNavbar} className="text-white hover:underline">
-            Menu
-          </button>
-        </div>
-        <div className={`md:flex md:space-x-4 ${isOpen ? "block" : "hidden"}`}>
-          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 sm:text-sm ">
+        <div className="hidden md:block">
+          <ul className="flex space-x-4">
             <li>
               <Link to="/" className="text-white hover:underline">
                 GENERAL
@@ -32,7 +54,7 @@ function Navbar() {
               </Link>
             </li>
             <li>
-              <Link to="/sports" className="text-white hover:underline">
+              <Link to="/spor" className="text-white hover:underline">
                 SPORTS
               </Link>
             </li>
@@ -58,15 +80,15 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <ul className="lg:flex gap-4 text-white">
+        <ul className="flex gap-4 text-white">
           <li>
             <div className="button-special text-black font-mono w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
-              <Link to="/pages/Login">LOG IN</Link>
+              <Link to="/pages/Login">LOGİN</Link>
             </div>
           </li>
           <li>
-            <div className="sm:mt-1 lg:mt-0 button-special text-black font-mono hover:underline w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
-              <Link to="/signup">SIGN UP</Link>
+            <div className="button-special text-black font-mono hover:underline w-20 h-7 border rounded-full text-center bg-white border-indigo-800">
+              <Link to="/signup">SİGN UP</Link>
             </div>
           </li>
         </ul>
